3b054e288e12b5a3c763ce185d76dcd2408db8ba
Michał Szaknis <m.szaknis@samsung.com>
2024-11-07 15:15:06 +0100

    Update veraison veryfier
    Signed-off-by: Michał Szaknis <m.szaknis@samsung.com>

--- lib/ratls/veraison-verifier/src/verifier.rs
+++ lib/ratls/veraison-verifier/src/verifier.rs
@@ -1,8 +1,10 @@
+use std::{fs, path::Path};
+
 use crate::{error::VeraisonTokenVeriferError, structs::{NewSessionresponse, SessionState, VerificationResults}};
 use ear::{Ear, TrustTier};
-use log::{error, info};
-use reqwest::blocking::Client;
-use base64::{Engine, engine::general_purpose::STANDARD as b64};
+use log::{error, info, trace};
+use reqwest::{blocking::Client, Certificate};
+use base64::{Engine, engine::general_purpose::URL_SAFE as b64};
 
 use ratls::{InternalTokenVerifier, RaTlsError};
 use rust_rsi::{verify_token, RealmClaims};
@@ -10,21 +12,29 @@ use rust_rsi::{verify_token, RealmClaims};
 #[derive(Debug)]
 pub struct VeraisonTokenVerifer {
     host: String,
-    pubkey: String
+    pubkey: String,
+    ca: Option<Certificate>
 }
 
 impl VeraisonTokenVerifer {
-    pub fn new(host: impl Into<String>, pubkey: impl Into<String>) -> Self {
-        Self {
+    pub fn new(host: impl Into<String>, pubkey: impl Into<String>, ca: Option<&[u8]>) -> Result<Self, VeraisonTokenVeriferError> {
+        Ok(Self {
             host: host.into(),
-            pubkey: pubkey.into()
-        }
+            pubkey: pubkey.into(),
+            ca: ca.map(|i| Certificate::from_der(i)).transpose()?
+        })
     }
 
     fn verify_attestation_results(&self, results: String) -> Result<(), VeraisonTokenVeriferError> {
         let ear = Ear::from_jwt_jwk(&results.as_str(), ear::Algorithm::ES256, self.pubkey.as_bytes())?;
+        trace!("Attestation results: {:#?}", ear.submods);
 
         for (name, appraisal) in ear.submods.into_iter() {
+            if name == "CCA_REALM" {
+                // TODO: we need to provision realms reference values to veraison to make it work
+                continue;
+            }
+
             if appraisal.status != TrustTier::Affirming {
                 error!("Submod {} does not affirm", name);
                 return Err(VeraisonTokenVeriferError::SubmoduleDoesNotAffirm(name));
@@ -38,7 +48,17 @@ impl VeraisonTokenVerifer {
         Ok(())
     }
 
-    fn verify_token(&self, client: Client, token: &[u8]) -> Result<(), VeraisonTokenVeriferError> {
+    fn verify_token(&self, token: &[u8]) -> Result<(), VeraisonTokenVeriferError> {
+        let mut builder = Client::builder();
+
+        if let Some(ca) = self.ca.as_ref() {
+            builder = builder.add_root_certificate(ca.clone());
+        } else {
+            builder = builder.danger_accept_invalid_certs(true);
+        }
+
+        let client = builder.build()?;
+
         let token_claims = verify_token(token, None)?;
         let realm_claims = RealmClaims::from_raw_claims(&token_claims.realm_claims.token_claims, &token_claims.realm_claims.measurement_claims)?;
 
@@ -66,7 +86,7 @@ impl VeraisonTokenVerifer {
 
         let verification_results = client.post(self.host.clone() + "/challenge-response/v1/" + session_path)
             .header("Accept", "application/vnd.veraison.challenge-response-session+json")
-            .header("Content-Type", "application/eat-collection; profile=http://arm.com/CCA-SSD/1.0.0")
+            .header("Content-Type", "application/eat-collection; profile=\"http://arm.com/CCA-SSD/1.0.0\"")
             .body(token.to_owned())
             .send()?
             .json::<VerificationResults>()?;
@@ -86,8 +106,7 @@ impl InternalTokenVerifier for VeraisonTokenVerifer {
     fn verify(&self, token: &[u8]) -> Result<(), RaTlsError> {
         std::thread::scope(|s| {
             s.spawn(move || {
-                let client = Client::new();
-                self.verify_token(client, token).map_err(|err| err.into())
+                self.verify_token(token).map_err(|err| err.into())
             }).join().expect("Thread failed")
         })
     }
